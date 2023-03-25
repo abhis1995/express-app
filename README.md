2697ce2d579089673960f19c4afbaac07d909d0a
abhis1995 <abhisankha.sahoo123@gmail.com>
2023-03-25 13:25:21 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # express-app
-# express-app
---
+++ terraform-ecs-cluster/ecr.tf
@@ -0,0 +1,10 @@
+resource "aws_ecr_repository" "repo" {
+  name                 = "app_repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  force_delete = true
+}
