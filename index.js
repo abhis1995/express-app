8458f20ee01714e57ca8e8915abfab5851daf1cd
abhis1995 <abhisankha.sahoo123@gmail.com>
2023-03-25 16:40:54 +0530

    file modifies

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World, my updated app');
+  res.send('Hello World');
 });
 
 /* istanbul ignore next */
